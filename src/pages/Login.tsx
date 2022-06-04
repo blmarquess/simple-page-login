8dac8f2837e4deb82a23cbd45e7e734a3f58f1ec
Bruno Marques <blmarques.dev@gmail.com>
2022-06-04 08:59:03 -0300

    Adding esential elements of page login input E-mail and Login State of manager transaction

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { useState } from 'react';
 
-export default function login() {
-  return <div>login</div>;
+const PSW_MIN = 8;
+const dotCom = /^[a-z0-9._-]+@[a-z0-9]+\.com$/;
+
+export default function Login() {
+  const [formState, setFormState] = useState({ user: '', psw: '' });
+
+  const stateUpdate = (key: string, value: string) =>
+    setFormState({ ...formState, [key]: value });
+
+  const isValidForm = () =>
+    formState.psw.length > PSW_MIN && dotCom.test(formState.user);
+
+  return (
+    <>
+      <span>Login:</span>
+      <input
+        type="email"
+        name="user"
+        autoFocus
+        placeholder="Email"
+        value={formState.user}
+        onChange={({ target: { name, value } }) => stateUpdate(name, value)}
+      />
+      <span>Senha:</span>
+      <input
+        value={formState.psw}
+        type="password"
+        name="psw"
+        placeholder="Senha"
+        onChange={({ target: { name, value } }) => stateUpdate(name, value)}
+      />
+
+        <button
+          onClick={() => console.log(formState.user)}
+          disabled={!isValidForm()}
+        >
+          Entrar
+        </button>
+
+    </>
+  );
 }
