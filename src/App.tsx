bf030bbc444d197fd53b985e86f9ba609958458b
Bruno Marques <blmarques.dev@gmail.com>
2022-06-04 11:29:32 -0300

    Adding router logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {  Routes,  Route,} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+    <Route path="/" element={<Login />} />
+      <Route path="logged" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const PSW_MIN = 8;
 const dotCom = /^[a-z0-9._-]+@[a-z0-9]+\.com$/;
@@ -7,19 +8,20 @@ export default function Login() {
   const [formState, setFormState] = useState({ user: '', psw: '' });
 
   const stateUpdate = (key: string, value: string) =>
-    setFormState({ ...formState, [key]: value });
+    setFormState((state) => ({ ...state, [key]: value }));
 
   const isValidForm = () =>
     formState.psw.length > PSW_MIN && dotCom.test(formState.user);
 
   return (
-    <>
+    <section className="formBox">
       <span>Login:</span>
       <input
         type="email"
         name="user"
         autoFocus
         placeholder="Email"
+        className="formInputs"
         value={formState.user}
         onChange={({ target: { name, value } }) => stateUpdate(name, value)}
       />
@@ -29,16 +31,21 @@ export default function Login() {
         type="password"
         name="psw"
         placeholder="Senha"
+        className="formInputs"
         onChange={({ target: { name, value } }) => stateUpdate(name, value)}
       />
-
+      <Link
+            style={{ display: "block" }}
+            to={`/logged`}
+            key="42"
+          >
         <button
           onClick={() => console.log(formState.user)}
           disabled={!isValidForm()}
         >
           Entrar
         </button>
-
-    </>
+      </Link>
+    </section>
   );
 }
